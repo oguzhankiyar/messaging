3fb7c358dfb0dcb7107d978049d44e777f6fdb03
Oğuzhan Kiyar <oguzhan.kiyar@outlook.com>
2018-08-06 00:08:03 +0300

    Initial commit.

---
+++ src/OK.Messaging.Api/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OK.Messaging.Api
+{
+    public class Startup
+    {
+        public Startup()
+        {
+
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
