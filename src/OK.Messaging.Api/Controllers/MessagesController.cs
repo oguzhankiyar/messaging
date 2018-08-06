2657f0b6c0c5a8be6dd1b7c785dd9de89b5e52ff
Oğuzhan Kiyar <oguzhan.kiyar@outlook.com>
2018-08-06 13:59:07 +0300

    MessagesController is completed.

---
+++ src/OK.Messaging.Api/Controllers/MessagesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OK.Messaging.Api.Requests;
+using OK.Messaging.Core.Managers;
+
+namespace OK.Messaging.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    public class MessagesController : BaseController
+    {
+        private readonly IMessageManager _messageManager;
+
+        public MessagesController(IMessageManager messageManager)
+        {
+            _messageManager = messageManager;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_messageManager.GetMessages(CurrentUserId.Value));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] SendMessageRequest request)
+        {
+            bool isSent = _messageManager.CreateMessage(CurrentUserId.Value, request.Username, request.Content);
+
+            if (!isSent)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ src/OK.Messaging.Api/Requests/SendMessageRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OK.Messaging.Api.Requests
+{
+    public class SendMessageRequest
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
