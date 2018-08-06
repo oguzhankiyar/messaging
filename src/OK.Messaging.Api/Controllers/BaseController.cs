221573755dd4ad590d1d3b3770f8b55bf51f02f8
Oğuzhan Kiyar <oguzhan.kiyar@outlook.com>
2018-08-06 03:03:07 +0300

    BaseController is created.

---
+++ src/OK.Messaging.Api/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OK.Messaging.Core.Managers;
+
+namespace OK.Messaging.Api.Controllers
+{
+    [ApiController]
+    public abstract class BaseController : ControllerBase
+    {
+        protected int? CurrentUserId
+        {
+            get
+            {
+                IAuthManager authManager = (IAuthManager)Request.HttpContext.RequestServices.GetService(typeof(IAuthManager));
+
+                return authManager.GetUserIdByPrincipal(Request.HttpContext.User);
+            }
+        }
+    }
+}
